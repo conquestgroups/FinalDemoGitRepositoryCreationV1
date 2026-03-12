b5a6e3eae8d3aa80b832c43745238877251a6787
conquestgroups <45680991+conquestgroups@users.noreply.github.com>
2026-03-12 19:20:42 +0530

    Add gitclassfetch.cs with response data structure

---
+++ gitclassfetch.cs
@@ -0,0 +1,14 @@
+{
+    "data": {
+        "client_id": "pan_mjIfnnJgbL",
+        "pan_number": "AABCZ6645E",
+        "full_name": "ZUELPAY SERVICES PRIVATE LIMITED",
+        "category": "company"
+    },
+    "status_code": 200,
+    "success": true,
+    "message": null,
+    "message_code": "success",
+    "result": 0,
+    "msg": "success"
+}
